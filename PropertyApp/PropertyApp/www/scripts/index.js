c27a77464fa59d0049327da9e53b2f4927ef2597
ChingYenHuang <chingyenhuang@gmail.com>
2016-03-22 15:16:47 +0200

    Included JQuery and JQuery.Mobile plugin

--- PropertyApp/PropertyApp/www/scripts/index.js
+++ PropertyApp/PropertyApp/www/scripts/index.js
@@ -9,6 +9,7 @@
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
+
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
